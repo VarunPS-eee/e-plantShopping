b50f329254b8a68400da38df8a3c4d91f3b89c32
VarunPS-eee <varunvinay22@gmail.com>
2025-03-17 11:09:19 -0400

    All changes done including cart items quantity update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,7 +7,9 @@ function ProductList({ onHomeClick }) {
     const dispatch = useDispatch();
 
     const cart = useSelector((state) => state.cart.items);
-
+    const numberOfitemsInCart =cart.reduce((sum,item)=>{
+        return sum + item.quantity; 
+     },0)
 
 
     const [showCart, setShowCart] = useState(false);
@@ -289,6 +291,9 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
+
+                    <div style={{position : 'absolute', right :'43px'}}>{numberOfitemsInCart}</div>
+
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
@@ -317,13 +322,13 @@ function ProductList({ onHomeClick }) {
                                                     disabled={isItemInCart}
                                                     style={
                                                         {
-                                                            backgroundColor : isItemInCart? "gray":"#28a745",
-                                                            color : "white",
-                                                            cursor : isItemInCart ? "not-allowed" : "pointer"
+                                                            backgroundColor: isItemInCart ? "gray" : "#28a745",
+                                                            color: "white",
+                                                            cursor: isItemInCart ? "not-allowed" : "pointer"
                                                         }
                                                     }
                                                 >
-                                                    {isItemInCart ? "Added already" : "Add to cart"}
+                                                    {isItemInCart ? "Added to cart" : "Add to cart"}
                                                 </button>
 
                                             </div>
